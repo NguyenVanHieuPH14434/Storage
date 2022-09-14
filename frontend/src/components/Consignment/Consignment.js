c235989f87efb9629e730913b5f11deb9b9107dd
rogerkieu <kieuthaian0212@gmail.com>
2022-09-14 12:38:17 +0700

    .

--- frontend/src/components/Consignment/Consignment.js
+++ frontend/src/components/Consignment/Consignment.js
@@ -5,17 +5,22 @@ import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import { Row, Col } from "react-bootstrap";
 import "./Consignment.scss";
+import ModalCreateConsignment from "./ModalCreateConsignment";
+import ModalUpdateConsignment from "./ModalUpdateConsignment";
 
 const Consignment = () => {
-  const [show1, setShow1] = useState(false);
+  const [showModalCreateConsignment, setShowModalCreateConsignment] =
+    useState(false);
+  const [showModalUpdateConsignment, setShowModalUpdateConsignment] =
+    useState(false);
+  const handleShowCreateConsignment = () => {
+    setShowModalCreateConsignment(true);
+  };
 
-  const handleClose1 = () => setShow1(false);
-  const handleShow1 = () => setShow1(true);
+  const handleShowUpdateConsignment = () => {
+    setShowModalUpdateConsignment(true);
+  };
 
-  const [show2, setShow2] = useState(false);
-
-  const handleClose2 = () => setShow2(false);
-  const handleShow2 = () => setShow2(true);
   return (
     <div className="consignment">
       <p className="title">Danh sách nhập kho</p>
@@ -30,7 +35,7 @@ const Consignment = () => {
             placeholder="Nhâp tên hàng hóa"
             className="fip"
           />
-          <Button variant="success" onClick={handleShow1}>
+          <Button variant="success" onClick={handleShowCreateConsignment}>
             THÊM MỚI
           </Button>
         </Form.Group>
@@ -56,7 +61,11 @@ const Consignment = () => {
             <td>CTTP dược phẩn trung ương 2</td>
             <td>21-08-2022</td>
             <td>
-              <Button variant="primary" className="btn" onClick={handleShow2}>
+              <Button
+                variant="primary"
+                className="btn"
+                onClick={handleShowUpdateConsignment}
+              >
                 Sửa
               </Button>
               <Button variant="danger">Xóa</Button>
@@ -64,108 +73,18 @@ const Consignment = () => {
           </tr>
         </tbody>
       </Table>
-      <Modal size="lg" show={show1} onHide={handleClose1}>
-        <Modal.Header closeButton>
-          <Modal.Title className="total">Thêm Hàng Hóa Nhập Kho</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Row>
-            <Col>
-              <Row>
-                <Form.Label>Tên Hàng Hóa</Form.Label>
-                <Form.Control
-                  type="text"
-                  style={{ width: "90%", marginLeft: "3%" }}
-                />
-              </Row>
-              <Row>
-                <Form.Label>Số Lô</Form.Label>
-                <Form.Control
-                  type="text"
-                  style={{ width: "90%", marginLeft: "3%" }}
-                />
-              </Row>
-            </Col>
-            <Col>
-              <Row>
-                <Form.Label>Nhà Cung Cấp</Form.Label>
-                <Form.Select
-                  aria-label="Default select example"
-                  style={{ width: "90%", marginLeft: "3%" }}
-                >
-                  <option>Chọn nhà Cung Cấp</option>
-                  <option value="1">CTTP dược phẩn trung ương 2</option>
-                </Form.Select>
-              </Row>
-              <Row>
-                <Form.Label>Ngày Nhập Kho</Form.Label>
-                <Form.Control
-                  type="date"
-                  style={{ width: "90%", marginLeft: "3%" }}
-                />
-              </Row>
-            </Col>
-          </Row>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="success">THÊM MỚI</Button>
-          <Button variant="danger" onClick={handleClose1}>
-            QUAY LẠI
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      <Modal size="lg" show={show2} onHide={handleClose2}>
-        <Modal.Header closeButton>
-          <Modal.Title className="total">
-            Sửa Thông Tin Hàng Hóa
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Row>
-            <Col>
-              <Row>
-                <Form.Label>Tên Hàng Hóa</Form.Label>
-                <Form.Control
-                  type="text"
-                  style={{ width: "90%", marginLeft: "3%" }}
-                />
-              </Row>
-              <Row>
-                <Form.Label>Số Lô</Form.Label>
-                <Form.Control
-                  type="text"
-                  style={{ width: "90%", marginLeft: "3%" }}
-                />
-              </Row>
-            </Col>
-            <Col>
-              <Row>
-                <Form.Label>Nhà Cung Cấp</Form.Label>
-                <Form.Select
-                  aria-label="Default select example"
-                  style={{ width: "90%", marginLeft: "3%" }}
-                >
-                  <option>Chọn nhà Cung Cấp</option>
-                  <option value="1">CTTP dược phẩn trung ương 2</option>
-                </Form.Select>
-              </Row>
-              <Row>
-                <Form.Label>Ngày Nhập Kho</Form.Label>
-                <Form.Control
-                  type="date"
-                  style={{ width: "90%", marginLeft: "3%" }}
-                />
-              </Row>
-            </Col>
-          </Row>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="success">CẬP NHẬT</Button>
-          <Button variant="danger" onClick={handleClose2}>
-            HỦY
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      {
+        <ModalCreateConsignment
+          show={showModalCreateConsignment}
+          setShow={setShowModalCreateConsignment}
+        />
+      }
+      {
+        <ModalUpdateConsignment
+          show={showModalUpdateConsignment}
+          setShow={setShowModalUpdateConsignment}
+        />
+      }
     </div>
   );
 };
