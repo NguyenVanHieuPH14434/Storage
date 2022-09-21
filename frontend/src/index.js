f0fa1682267518835606bc1b38ec90a77b1117a2
Hieunvph14434 <nguyenvanhieu2001gl@gmail.com>
2022-09-21 16:09:09 +0700

    cm1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import './font-awesome/css/font-awesome.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
